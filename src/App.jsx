2f6cc7b6f5007e194114b640364f745cf88a5dcb
Ana Mena <amena@wustl.edu>
2025-03-31 15:40:00 -0500

    added name

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} classNeame="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR + Ana
+          Edit <code>src/App.jsx</code> and save to test HMR + Ana Mena
         </p>
       </div>
       <p className="read-the-docs">
