34ecb582f99b133b7a70cd1a61210cd51c1bf6de
Sam Tihen <git@samtihen.com>
2025-04-01 08:23:45 -0500

    put stuff back to get you back on track

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
+import "./index.css";
 
 import App from "./App";
 
