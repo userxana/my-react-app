05f73b7daede882fdaa205c22f6db3c18531a035
Ana Mena <amena@wustl.edu>
2025-04-01 16:55:41 -0500

    built the borard using fragments and divs

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,31 @@
 //   return <button className="square">{value}</button>;
 // }
 
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Board() {
+  // className="square" is a button property or prop that tells CSS how to style the button
+  // return <button className="square">X</button>;
+
+  // you can use Fragments (<> and </>) to wrap multiple adjacent JSX elements
+      // The return JavaScript keyword means whatever comes after is returned as a value to the caller of the function.
+      return (
+        <>
+          <div className="board-row">
+            <button className="square">1</button>
+            <button className="square">2</button>
+            <button className="square">3</button>
+          </div>
+          <div className="board-row">
+            <button className="square">4</button>
+            <button className="square">5</button>
+            <button className="square">6</button>
+          </div>
+          <div className="board-row">
+            <button className="square">7</button>
+            <button className="square">8</button>
+            <button className="square">9</button>
+          </div>
+        </>
+      );
 }
 
 
