92840d886940301918e6458bb2606130f30c5614
Ana Mena <amena@wustl.edu>
2025-04-01 17:00:06 -0500

    passed square number data thru props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@
 //   return <button className="square">{value}</button>;
 // }
 
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+
 export default function Board() {
   // className="square" is a button property or prop that tells CSS how to style the button
   // return <button className="square">X</button>;
@@ -10,21 +16,21 @@ export default function Board() {
       // The return JavaScript keyword means whatever comes after is returned as a value to the caller of the function.
       return (
         <>
-          <div className="board-row">
-            <button className="square">1</button>
-            <button className="square">2</button>
-            <button className="square">3</button>
-          </div>
-          <div className="board-row">
-            <button className="square">4</button>
-            <button className="square">5</button>
-            <button className="square">6</button>
-          </div>
-          <div className="board-row">
-            <button className="square">7</button>
-            <button className="square">8</button>
-            <button className="square">9</button>
-          </div>
+            <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
         </>
       );
 }
