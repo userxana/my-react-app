1f8bb379aecf81198e7b900382dc65e40295f149
Ana Mena <amena@wustl.edu>
2025-04-01 17:09:08 -0500

    added interactive component so click on square = X

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,27 @@
 // }
 
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+import { useState } from 'react';
+
+function Square() {
+
+  // value stores the value and setValue is a function that can be used to change the value. 
+  // The null passed to useState is used as the initial value for this state variable, 
+  // so value here starts off equal to null
+  const [value, setValue] = useState(null);
+
+  // If you click on a square now, you should see a log saying "clicked!" in the Console 
+  function handleClick() {
+    setValue('X');
+  }
+  return (
+  <button
+    className="square"
+    onClick={handleClick}
+  >
+    {value}
+  </button>
+  );
 }
 
 
@@ -13,26 +32,26 @@ export default function Board() {
   // return <button className="square">X</button>;
 
   // you can use Fragments (<> and </>) to wrap multiple adjacent JSX elements
-      // The return JavaScript keyword means whatever comes after is returned as a value to the caller of the function.
-      return (
-        <>
-            <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+  // The return JavaScript keyword means whatever comes after is returned as a value to the caller of the function.
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
-        </>
-      );
+    </>
+  );
 }
 
 
