1a068e373e36c0108fb9a5e2439f44ee6b6bd892
Ana Mena <amena@wustl.edu>
2025-04-01 18:06:05 -0500

    deleted more old code comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,3 @@
-// import React, { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-
-// import App from "./App";
-
-// const root = createRoot(document.getElementById("root"));
-// root.render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
-
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
